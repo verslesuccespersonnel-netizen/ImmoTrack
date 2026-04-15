953ca75351b0424e45b398e3f3de84581c242e3d
verslesuccespersonnel-netizen <verslesuccespersonnel@gmail.com>
2026-04-15 21:57:13 +0200

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPA_URL = process.env.REACT_APP_SUPABASE_URL  || ''
-const SUPA_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY || ''
-
-export const configured = !!(SUPA_URL && SUPA_KEY)
-export const supabase   = createClient(SUPA_URL, SUPA_KEY)
